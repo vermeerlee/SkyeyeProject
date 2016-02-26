cf6e57d5a86e71634a8779ac4a35021769a7a648
xmh <xmh19936688@163.com>
2016-02-26 09:00:30 +0800

    step:ChatDemoUI.MainActivity.MyConnectionListener.onConnected(）：添加好友被同意后

--- ChatDemoUI/src/com/easemob/chatuidemo/activity/LoginActivity.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/activity/LoginActivity.java
@@ -177,7 +177,7 @@ public class LoginActivity extends BaseActivity {
 				Intent intent = new Intent(LoginActivity.this,
 						MainActivity.class);
 				startActivity(intent);
-				
+
 				finish();
 			}
 
